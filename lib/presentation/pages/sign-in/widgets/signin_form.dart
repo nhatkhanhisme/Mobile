298dceb64a09412b58105893bdcd99d74d3639ba
Sir <duyhuu1109@gmail.com>
2025-03-06 21:26:38 +0700

    feat: add app launcher icon

---
+++ lib/presentation/pages/sign-in/widgets/signin_form.dart
@@ -0,0 +1,9 @@
+// import 'package:flutter/material.dart';
+
+// Widget _emailField() {
+//   return TextField(decoration: const InputDecoration(hintText: "Email"));
+// }
+
+// Widget _passwordField() {
+//   return TextField(decoration: const InputDecoration(hintText: "Password"));
+// }
