196b09ffee4a05c8f651d285fac15d4a2ddcd6a1
QUOC HAI <haiquocntp@gmail.com>
2025-03-19 20:38:25 +0700

    chore: delete macos folder

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.uit.lacquer"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
