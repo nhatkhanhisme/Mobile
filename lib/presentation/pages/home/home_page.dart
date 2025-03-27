f2b2906ba8c9963a531a653078e2b7169702618c
Sir <duyhuu1109@gmail.com>
2025-03-27 22:25:33 +0700

    feat: add App bar and Learn today card

--- lib/presentation/pages/home/home_page.dart
+++ lib/presentation/pages/home/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:lacquer/config/theme.dart';
 import 'package:lacquer/features/auth/data/home_item_list.dart';
 import 'package:lacquer/presentation/widgets/bottom_nav_bar.dart';
@@ -11,31 +12,145 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: CustomTheme.lightbeige,
-      appBar: AppBar(
-        backgroundColor: CustomTheme.cinnabar,
-        title: const Text('Home Screen'),
-      ),
-
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: GridView.builder(
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            crossAxisSpacing: 16,
-            mainAxisSpacing: 16,
-            childAspectRatio: 0.8,
+      body: Column(
+        children: [
+          Stack(
+            clipBehavior: Clip.none,
+            children: [_buildAppBar(), _buildLearnedTodayCard()],
           ),
-          itemCount: homeItems.length,
-          itemBuilder: (context, index) {
-            return HomeItem(
-              imagePath: homeItems[index].imagePath,
-              title: homeItems[index].title,
-              backgroundColor: homeItems[index].backgroundColor,
-            );
-          },
-        ),
+          const SizedBox(height: 80),
+          Expanded(child: _buildGridView()),
+        ],
       ),
       bottomNavigationBar: const BottomNavBar(selectedIndex: 0),
+    );
+  }
+
+  Widget _buildAppBar() {
+    return ClipRRect(
+      borderRadius: const BorderRadius.only(
+        bottomLeft: Radius.circular(8),
+        bottomRight: Radius.circular(8),
+      ),
+      child: Container(
+        height: 170,
+        color: CustomTheme.cinnabar,
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 20),
+        child: Row(
+          children: [
+            const SizedBox(width: 10),
+            Container(
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                border: Border.all(color: Colors.white, width: 2),
+              ),
+              child: const CircleAvatar(
+                backgroundImage: AssetImage('assets/images/avatar.jpg'),
+                radius: 20,
+              ),
+            ),
+            const SizedBox(width: 10),
+            const Text(
+              'Vu Phan',
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const Spacer(),
+            IconButton(
+              icon: const Icon(FontAwesomeIcons.bell, color: Colors.white),
+              onPressed: () {},
+            ),
+            const SizedBox(width: 10),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildLearnedTodayCard() {
+    return Positioned(
+      left: 16,
+      right: 16,
+      top: 130,
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          color: CustomTheme.lightbeige,
+          borderRadius: BorderRadius.circular(12),
+          boxShadow: [
+            BoxShadow(
+              color: const Color.fromRGBO(0, 0, 0, 0.1),
+              blurRadius: 6,
+              offset: const Offset(0, 2),
+            ),
+          ],
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              "Learned today",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+            ),
+            const SizedBox(height: 8),
+            Row(
+              children: [
+                const Text(
+                  "46min",
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+                const Text(" / 60min"),
+                const Spacer(),
+                Container(
+                  width: 150,
+                  height: 5,
+                  decoration: BoxDecoration(
+                    color: Colors.grey[300],
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Stack(
+                    children: [
+                      Container(
+                        width: 110,
+                        height: 5,
+                        decoration: BoxDecoration(
+                          color: Colors.orange,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildGridView() {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: GridView.builder(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 16,
+          mainAxisSpacing: 16,
+          childAspectRatio: 0.8,
+        ),
+        itemCount: homeItems.length,
+        itemBuilder: (context, index) {
+          return HomeItem(
+            imagePath: homeItems[index].imagePath,
+            title: homeItems[index].title,
+            backgroundColor: homeItems[index].backgroundColor,
+          );
+        },
+      ),
     );
   }
 }
